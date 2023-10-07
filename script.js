ea62161134e75ca1be4bbee13392722b812932ed
tomek.larysz <ttomek.larysz@gmail.com>
2023-10-07 13:33:59 +0200

    hover color

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i <= 16; i++) {
+function createGrid() {
+    for (let i = 0; i <= 16; i++) {
     
-    const column = document.createElement('div');
-    column.setAttribute('class', 'column');
-    container.appendChild(column);
+        const column = document.createElement('div');
+        column.setAttribute('class', 'column');
+        container.appendChild(column);
+        
+        for (let j = 0; j <= 16; j++) {
+            const row = document.createElement('div');
+            row.setAttribute('class', 'row');
+            column.appendChild(row);
+        }
     
-    for (let j = 0; j <= 16; j++) {
-        const row = document.createElement('div');
-        row.setAttribute('class', 'row');
-        column.appendChild(row);
     }
-
-    console.log(i);
 }
+
+function color() {
+    cells = document.querySelectorAll('.row');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'blue';
+        });
+    });
+}
+
+createGrid();
+color();
